7930df8e13def6e7bc9e724187c927c6e37c0a68
John Skubic <jskubic@purdue.edu>
2017-04-26 16:35:03 -0700

    Added bash setup script

---
+++ admin/setup_env.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z $SFF_ADMIN ]; then
+  rootdir="${BASH_SOURCE[0]}";
+  if ([ -h "${rootdir}" ]) then
+    while([ -h "${rootdir}" ]) do rootdir=`readlink "${rootdir}"`; done
+  fi
+  pushd . > /dev/null
+  cd `dirname ${rootdir}` > /dev/null
+  rootdir=`pwd`;
+  popd  > /dev/null
+else
+  rootdir=$SFF_ADMIN
+fi
+
+export PATH=$PATH:$rootdir/veripool
+export PATH=$PATH:$rootdir/waf/waf-current
+export WAFDIR=$rootdir/waf/waf-current
+
+
